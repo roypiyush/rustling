febd531c2567a747b2872c9cc2977e57fe0d9fa9
Piyush Roy <piyush2k13@gmail.com>
2024-02-03 00:17:56 +0530

    ssh client somewhat working

--- main/src/bin/ssh_client.rs
+++ main/src/bin/ssh_client.rs
@@ -1,18 +1,21 @@
 use std::{
-    io::{self, Write}, sync::{Arc, Mutex}, thread::{self}
+    io::{self, Write},
+    sync::{Arc, Mutex},
+    thread::{self},
 };
 
+use std::sync::mpsc;
 
 use ssh::{self, TerminalSize};
 
 fn main() {
-    
     let mut cmd_args = std::env::args();
     cmd_args.next(); // skip this file
 
     let host_with_port = cmd_args.next();
     let username = cmd_args.next();
     let password = cmd_args.next();
+    let prompt = cmd_args.next().unwrap(); // prompt could be auto detected
 
     let reader = thread::Builder::new().name("reader".to_string());
     let writer = thread::Builder::new().name("writer".to_string());
@@ -34,32 +37,70 @@ fn main() {
         .unwrap()
         .run_backend();
 
-
     let tv = TerminalSize::from(80, 40);
-    let channel = Arc::new(Mutex::new(session.open_channel().unwrap().shell(tv).unwrap()));
+    let channel = Arc::new(Mutex::new(session.open_shell_terminal(tv).unwrap()));
 
     let write_channel = Arc::clone(&channel);
     let read_channel = Arc::clone(&channel);
 
-    reader.spawn(move || loop {
+    let is_prompt = Arc::new(Mutex::new(false));
+    let is_exit = Arc::new(Mutex::new(false));
+    let is_exit_reader = Arc::clone(&is_exit);
+    let is_exit_writer = Arc::clone(&is_exit);
 
-        let data = read_channel.try_lock().unwrap().read().unwrap();
-        print!("{}", String::from_utf8(data).unwrap());
-        io::stdout().flush().unwrap();
-        
-    }).unwrap();
+    let (tx, rx) = mpsc::channel();
 
-    let join_handle = writer.spawn(move || loop {
-        let mut command = String::new();
-        let stdin = io::stdin();
-        stdin.read_line(&mut command).unwrap();
-        //write_channel.try_lock().unwrap().write(command.as_bytes()).unwrap();
-        if let Ok(mut write) = write_channel.try_lock() {
-            write.write(command.as_bytes()).unwrap();
-        } else {
-            println!("Error Occured due to locking. Fix this to make it work");
-        }
-    }).unwrap();
+    let join_handle_reader = reader
+        .spawn(move || loop {
+            if *is_prompt.try_lock().unwrap() {
+                rx.recv().unwrap();
+                if *is_exit_reader.try_lock().unwrap() {
+                    break;
+                }
 
-    join_handle.join().unwrap();
+                *is_prompt.try_lock().unwrap() = false;
+
+            } else {
+                let data = read_channel.try_lock().unwrap().read().unwrap();
+                let string_data = String::from_utf8(data).unwrap();
+                print!("{}", string_data);
+                io::stdout().flush().unwrap();
+
+                // Idea is that prompt marks end of output
+                if string_data.contains(&prompt) {
+                    *is_prompt.try_lock().unwrap() = true;
+                }
+            }
+        })
+        .unwrap();
+
+    let join_handle_writer = writer
+        .spawn(move || loop {
+            if *is_exit_writer.try_lock().unwrap() {
+                break;
+            }
+
+            let mut cmd = String::new();
+            let stdin = io::stdin();
+            stdin.read_line(&mut cmd).unwrap();
+            
+            if let Ok(mut write) = write_channel.try_lock() {
+
+                if cmd.contains("exit") {
+                    *is_exit_writer.try_lock().unwrap() = true;
+                }
+                write.write(cmd.as_bytes()).unwrap();    
+                tx.send("f").unwrap();
+
+            } else {
+                // Error Scenarios [add more as required]
+                // 1. Multiple line command where prompt disappears
+                println!("Error Occured due to locking in reader thread. A bug is found.");
+            }
+        })
+        .unwrap();
+
+    join_handle_writer.join().unwrap();
+    join_handle_reader.join().unwrap();
+    session.close();
 }
