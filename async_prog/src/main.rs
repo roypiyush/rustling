88988319c2c0717e6a737b6d482131a1562208ba
piyush <piyush@no-reply.github.com>
2025-06-10 01:18:48 +0530

    sample cpu test

--- async_prog/src/main.rs
+++ async_prog/src/main.rs
@@ -1,9 +1,34 @@
-async fn my_async() {
-    println!("Hello, world!");
+use std::time::Instant;
+use tokio::task;
+
+fn heavy_cpu_task(id: usize) {
+    let mut count = 0;
+    for _ in 0..5000 {
+        count += 1;
+    }
+
+    println!("Task {id} done {count}");
 }
 
-#[async_std::main]
+#[tokio::main]
 async fn main() {
-    let my_async = my_async();
-    my_async.await;
+    let now = Instant::now();
+
+    let logical = num_cpus::get();
+    let physical = num_cpus::get_physical();
+    println!("CPU {logical}T / {physical}C");
+
+    let mut handles = vec![];
+    for i in 0..logical {
+        handles.push(task::spawn_blocking(move || {
+            heavy_cpu_task(i);
+        }));
+    }
+
+    // Wait for all tasks to complete
+    for h in handles {
+        let _ = h.await;
+    }
+
+    println!("All tasks completed in {:.2?}", now.elapsed());
 }
